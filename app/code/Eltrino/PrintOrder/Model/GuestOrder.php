f72f00b83dd27df872df030bf3cd0bdfbc3faf9e
Vladimir Polischuk <vladimir.polischuk@eltrino.com>
2015-03-13 18:42:37 +0200

    Add MIT license

--- app/code/Eltrino/PrintOrder/Model/GuestOrder.php
+++ app/code/Eltrino/PrintOrder/Model/GuestOrder.php
@@ -3,20 +3,26 @@
 /**
  * Print Order Confirmation as Guest
  *
- * LICENSE
+ * Copyright (c) 2012 Eltrino LLC. (http://eltrino.com). All rights reserved.
  *
- * This source file is subject to the Eltrino LLC EULA
- * that is bundled with this package in the file LICENSE_EULA.txt.
- * It is also available through the world-wide-web at this URL:
- * http://eltrino.com/license-eula.txt
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to license@eltrino.com so we can send you a copy immediately.
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
  *
- * @category    Eltrino
- * @package     Eltrino_PrintOrder
- * @copyright   Copyright (c) 2012 Eltrino LLC. (http://eltrino.com)
- * @license     http://eltrino.com/license-eula.txt  Eltrino LLC EULA
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
  */
 namespace Eltrino\PrintOrder\Model;
 
